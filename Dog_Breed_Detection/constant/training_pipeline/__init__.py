f9f6f27b56dc19f0bfd2791901dcf150a69989e8
Singhpriyanshu2907 <priyanshus2907@gmail.com>
2025-01-06 19:14:14 +0530

    Data Validation Stage Completed

--- Dog_Breed_Detection/constant/training_pipeline/__init__.py
+++ Dog_Breed_Detection/constant/training_pipeline/__init__.py
@@ -8,3 +8,16 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
 DATA_DOWNLOAD_URL: str = "https://app.roboflow.com/ds/6ZG8fHCaw6?key=n1fMl7ipej"
+
+
+
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["test", "train", "valid", "data.yaml"]
--- Dog_Breed_Detection/pipeline/training_pipeline.py
+++ Dog_Breed_Detection/pipeline/training_pipeline.py
@@ -2,15 +2,16 @@ import sys, os
 from Dog_Breed_Detection.logger import logging
 from Dog_Breed_Detection.exception import AppException
 from Dog_Breed_Detection.components.data_ingestion import DataIngestion
-from Dog_Breed_Detection.entity.config_entity import (DataIngestionConfig)
-from Dog_Breed_Detection.entity.artifacts_entity import (DataIngestionArtifact)
+from Dog_Breed_Detection.components.data_validation import DataValidation
+from Dog_Breed_Detection.entity.config_entity import (DataIngestionConfig,DataValidationConfig)
+from Dog_Breed_Detection.entity.artifacts_entity import (DataIngestionArtifact,DataValidationArtifact)
 
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
-        #self.data_validation_config = DataValidationConfig()
+        self.data_validation_config = DataValidationConfig()
         #self.model_trainer_config = ModelTrainerConfig()
 
 
@@ -38,12 +39,39 @@ class TrainPipeline:
             raise AppException(e, sys)
         
 
+    def start_data_validation(
+        self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config,
+            )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise AppException(e, sys) from e
+        
+
 
 
 
     def run_pipeline(self) -> None:
             try:
                 data_ingestion_artifact = self.start_data_ingestion()
+                data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact
+            )
 
             
             
