b5d51fc14f16d2b4c4477ba7790173fd8d2715fd
Singhpriyanshu2907 <priyanshus2907@gmail.com>
2025-01-09 06:30:35 +0530

    model updated

--- app.py
+++ app.py
@@ -1,9 +1,77 @@
-import sys,os
+import sys, os
+import shutil
+import traceback
+import subprocess
 from Dog_Breed_Detection.pipeline.training_pipeline import TrainPipeline
 from Dog_Breed_Detection.utils.main_utils import decodeImage, encodeImageIntoBase64
-from flask import Flask, request, jsonify, render_template,Response
-#from flask_cors import CORS, cross_origin
+from flask import Flask, request, jsonify, render_template, Response
+from flask_cors import CORS, cross_origin
+from Dog_Breed_Detection.constant.application import APP_HOST, APP_PORT
+
+app = Flask(__name__)
+CORS(app)
+
+class ClientApp:
+    def __init__(self):
+        self.filename = "inputImage.jpg"
+
+def remove_folder(folder_path):
+    """Helper function to remove directories."""
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)
+
+@app.route("/train")
+def trainRoute():
+    obj = TrainPipeline()
+    obj.run_pipeline()
+    return "Training Successful!!" 
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=['POST','GET'])
+@cross_origin()
+def predictRoute():
+    try:
+        image = request.json['image']
+        decodeImage(image, clApp.filename)
+
+        os.system("cd yolov5/ && python detect.py --weights best.pt --img 416 --conf 0.10 --source ../data/inputImage.jpg")
+
+        opencodedbase64 = encodeImageIntoBase64("yolov5/runs/detect/exp/inputImage.jpg")
+        result = {"image": opencodedbase64.decode('utf-8')}
+        #os.remove("yolov5/runs")
+
+    except ValueError as val:
+        print(val)
+        return Response("Value not found inside  json data")
+    except KeyError:
+        return Response("Key value error incorrect key passed")
+    except Exception as e:
+        print(e)
+        result = "Invalid input"
+
+    return jsonify(result)
 
 
-obj = TrainPipeline()
-obj.run_pipeline()
+@app.route("/live", methods=['GET'])
+@cross_origin()
+def predictLive():
+    try:
+        os.system("cd yolov5 && python detect.py --weights best.pt --img 416 --conf 0.05 --source 0")
+
+        # Remove output directory using Python
+        remove_folder("yolov5/runs")
+        return "Camera starting!!" 
+
+    except ValueError as val:
+        print(val)
+        return Response("Value not found inside JSON data")
+    except Exception as e:
+        print(e)
+        return Response("An error occurred while starting the camera")
+
+if __name__ == "__main__":
+    clApp = ClientApp()
+    app.run(host=APP_HOST, port=APP_PORT)
